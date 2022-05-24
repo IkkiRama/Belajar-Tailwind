367e64611cb8443cd6dc31873643b24df8ab7261
IkkiRama <georgeikkirama@gmail.com>
2022-05-24 21:16:50 +0700

    Functions & Directive

---
+++ 10 Function dan Directive/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        ikki: "#ccc",
+      },
+    },
+  },
+  plugins: [],
+};
